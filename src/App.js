a7f00351e181666e5b082ea3aee447c98110a7f0
Niraj Limbu <nlimbu@yahoo.com>
2022-03-17 14:19:57 -0400

    control state with a controlled component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import Nav from './components/Nav'
 import About from './components/About'
 import Gallery from './components/Gallery'
+import ContactForm from './components/Contact'
 
 function App() {
   const [categories] = useState([
@@ -20,8 +21,10 @@ function App() {
         currentCategory={currentCategory}
         ></Nav>
       <main>
+      {/* invoke components */}
+      <ContactForm></ContactForm>
       <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        <About></About>        
       </main>
     </div>
   );
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,31 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+
 
 function ContactForm() {
-    // JSX
+    // Hook to manage the form data, set the initial state to empty strings to get the following expression
+    const [formState, setFormState] = useState({name: '', email: '', message: ''});
 
+    // destructure the formState object into its named properties
+    const {name, email, message} = formState;
+
+
+    function handleChange(e) {
+        // spread operator ...
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+    
     return (
         <section>
             <h1>Contact me</h1>
-            <form id='contact-form'>
+            <form id='contact-form' onSubmit={handleSubmit}>
                 {/* name input */}
                 <div>
                     <label htmlFor='name'>Name:</label>
-                    <input type='text' name='name' />
+                    {/* handleChange will sync the internal state of the component formState */}
+                    <input type='text' defaultValue={name} onChange={handleChange} name='name' />
                 </div>
                 {/* email input */}
                 <div>
                     <label htmlFor='email'>Email address:</label>
-                    <input type='email' name='email' />
+                    <input type='email' defaultValue={email} onChange={handleChange} name='email' />
                 </div>
                 {/* message text area */}
                 <div>
                     <label htmlFor='message'>Message:</label>
-                    <textarea name='message' rows='5' />
+                    <textarea name='message' defaultValue={message} onChange={handleChange} rows='5' />
                 </div>
                 <button type='submit'>Submit</button>
             </form>
         </section>
     )
 }
+
+
 
 export default ContactForm;
